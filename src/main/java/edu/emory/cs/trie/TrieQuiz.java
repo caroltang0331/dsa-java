23c042309c4a284250e383155602d37d0e0aa509
caroltang0331 <carol.tang@emory.edu>
2020-10-16 13:53:29 +0800

    quiz5_1

---
+++ src/main/java/edu/emory/cs/trie/TrieQuiz.java
@@ -0,0 +1,40 @@
+package edu.emory.cs.trie;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TrieQuiz extends Trie<Integer> {
+    /**
+     * PRE: this trie contains all country names as keys and their unique IDs as values
+     * (e.g., this.get("United States") -> 0, this.get("South Korea") -> 1).
+     * @param input the input string in plain text
+     *              (e.g., "I was born in South Korea and raised in the United States").
+     * @return the list of entities (e.g., [Entity(14, 25, 1), Entity(44, 57, 0)]).
+     */
+    List<Entity> getEntities(String input) {
+        List<Entity> result = new ArrayList<Entity>();
+        String[] splitSpace = input.split(" ");
+        int[] indexCount = new int[splitSpace.length];
+        indexCount[0] = 0;
+        for (int i = 1; i < splitSpace.length; i++) {
+            indexCount[i] = splitSpace[i - 1].length() + indexCount[i - 1] + 1;
+        }
+
+        for (int i = 0; i < splitSpace.length; ) {
+            if (this.find(splitSpace[i]) != null) {
+                int beginIndex = indexCount[i];
+                if (this.contains(splitSpace[i])) result.add(new Entity(indexCount[i], indexCount[i + 1] - 1, this.get(splitSpace[i])));
+                while (i + 1 < splitSpace.length && this.find(splitSpace[i] + " " + splitSpace[i + 1]) != null) {
+                    splitSpace[i + 1] = splitSpace[i] + " " + splitSpace[i + 1];
+                    if (this.contains(splitSpace[i + 1]))
+                        result.add(new Entity(beginIndex, beginIndex + splitSpace[i + 1].length(), this.get(splitSpace[i + 1])));
+                    i++;
+                }
+            }
+            i++;
+        }
+
+        return result;
+    }}
+
---
+++ src/test/java/edu/emory/cs/trie/TrieQuizTest.java
@@ -0,0 +1,28 @@
+package edu.emory.cs.trie;
+
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+
+/** @author Jinho D. Choi */
+public class TrieQuizTest {
+    @Test
+    public void testGetEntities() {
+        final List<String> L = List.of("United States", "South Korea", "United States of America", "Costa");
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        String input = "Costa born in South Korea and raised in the United States of America ho";
+        List<Entity> entities = List.of(new Entity(0,5,3), new Entity(44, 57, 0), new Entity(44, 68, 2),new Entity(14, 25, 1));
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+}
