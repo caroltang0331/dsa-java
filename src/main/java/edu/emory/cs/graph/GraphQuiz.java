dfffcb1f5cd04fe42aa230f754653528a9a7cf6a
caroltang0331 <carol.tang@emory.edu>
2020-11-06 15:29:27 +0800

    quiz6onlytest2and3

---
+++ src/main/java/edu/emory/cs/graph/GraphQuiz.java
@@ -0,0 +1,44 @@
+package edu.emory.cs.graph;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/** @author Jinho D. Choi */
+public class GraphQuiz extends Graph {
+    public GraphQuiz(int size) { super(size); }
+    public GraphQuiz(Graph g) { super(g); }
+
+    /** @return the total number of cycles in this graph. */
+    public int numberOfCycles() {
+        int count = 0;
+        Deque<Integer> notVisited = IntStream.range(0, size()).boxed().collect(Collectors.toCollection(ArrayDeque::new));
+
+        while (!notVisited.isEmpty()) {
+            count += (containsCycleAux(notVisited.poll(), notVisited, new HashSet<>()));
+        }
+
+        return count;
+    }
+
+    private int containsCycleAux(int target, Deque<Integer> notVisited, Set<Integer> visited) {
+        int count = 0;
+        notVisited.remove(target);
+        visited.add(target);
+
+
+        for (Edge edge : getIncomingEdges(target)) {
+            if (visited.contains(edge.getSource())) count++;
+
+
+            else return count += (containsCycleAux(edge.getSource(), notVisited, new HashSet<>(visited)));
+        }
+        return count;
+
+
+    }
+
+}
---
+++ src/test/java/edu/emory/cs/graph/GraphTest.java
@@ -0,0 +1,65 @@
+package edu.emory.cs.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * @author Jinho D. Choi ({@code jinho.choi@emory.edu})
+ */
+public class GraphTest {
+    @Test
+    public void testContainsCycle() {
+        GraphQuiz graph = new GraphQuiz(5);
+
+//Test1
+        graph.setDirectedEdge(0, 1, 1);
+        graph.setDirectedEdge(0, 2, 1);
+        graph.setDirectedEdge(2, 1, 1);
+        graph.setDirectedEdge(2, 3, 1);
+        graph.setDirectedEdge(3, 4, 1);
+        graph.setDirectedEdge(4, 2, 1);
+
+//Test2
+        //graph.setDirectedEdge(2, 0, 1);
+
+
+
+//        graph.setDirectedEdge(0, 1, 1);
+//        graph.setDirectedEdge(1, 2, 1);
+//        graph.setDirectedEdge(2, 3, 1);
+//        graph.setDirectedEdge(3, 1, 1);
+//        graph.setDirectedEdge(3, 0, 1);
+//        graph.setDirectedEdge(1, 4, 1);
+
+
+
+
+
+
+
+        System.out.println(graph.numberOfCycles());
+        System.out.println(graph.toString());
+        assertTrue(graph.containsCycle());
+    }
+
+    @Test
+    public void testTopologicalSort() {
+        Graph graph = new Graph(8);
+
+        graph.setDirectedEdge(0, 3, 1);
+        graph.setDirectedEdge(0, 4, 1);
+        graph.setDirectedEdge(1, 3, 1);
+        graph.setDirectedEdge(2, 4, 1);
+        graph.setDirectedEdge(3, 5, 1);
+        graph.setDirectedEdge(3, 6, 1);
+        graph.setDirectedEdge(3, 7, 1);
+        graph.setDirectedEdge(4, 6, 1);
+        graph.setDirectedEdge(2, 7, 1);
+
+        assertEquals("[0, 1, 2, 3, 4, 5, 7, 6]", new Graph(graph).topological_sort(false).toString());
+        assertEquals("[0, 1, 3, 5, 2, 4, 6, 7]", new Graph(graph).topological_sort(true).toString());
+    }
+}
